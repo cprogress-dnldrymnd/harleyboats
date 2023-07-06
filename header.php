825b0f6a6888a0a38f8627fcdff350c64c7b0e5b
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-07-06 18:25:30 +0800

    changes

--- header.php
+++ header.php
@@ -61,8 +61,7 @@
 								<div class="offcanvas offcanvas-start" tabindex="-1" id="offCanvasMenu"
 									aria-labelledby="offCanvasMenuLabel">
 									<div class="offcanvas-header">
-										<h5 class="offcanvas-title" id="offCanvasMenuLabel">Offcanvas</h5>
-										<button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas"
+										<button type="button" class="btn-close text-reset d-block d-lg-none" data-bs-dismiss="offcanvas"
 											aria-label="Close"></button>
 									</div>
 									<div class="offcanvas-body">

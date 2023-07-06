b6a52870ace6a95c260298e66e6d1b242eca76eb
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-07-06 18:36:38 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -129,4 +129,14 @@ add_action('woocommerce_after_main_content', 'action_woocommerce_after_main_cont
 
 
 remove_action('woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart', 10);
-remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart');
+
+
+add_action('woocommerce_after_shop_loop_item', 'bbloomer_view_product_button', 10);
+
+function bbloomer_view_product_button()
+{
+	global $product;
+	$link = $product->get_permalink();
+	echo '<a href="' . $link . '" class="button addtocartbutton">View Product</a>';
+}
+remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 10);

d598cf591b7fdf4d36450c40a268c90370ecb43b
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-07-06 18:40:21 +0800

    changes

--- footer.php
+++ footer.php
@@ -4,8 +4,41 @@
 /* rendering the page and display the footer area/content
 /*-----------------------------------------------------------------------------------*/
 ?>
+
+<footer>
+  <div class="container">
+    <div class="row g-5">
+      <div class="col-sm-4 footer-column-1">
+        <?php if (is_active_sidebar('footer_column_1')) { ?>
+
+          <div class="column-holder  ">
+            <?php dynamic_sidebar('footer_column_1') ?>
+          </div>
+        </div>
+      <?php } ?>
+      <?php if (is_active_sidebar('footer_column_2')) { ?>
+
+        <div class="col-sm-4 footer-column-2">
+          <div class="column-holder">
+            <?php dynamic_sidebar('footer_column_2') ?>
+          </div>
+        </div>
+      <?php } ?>
+
+      <?php if (is_active_sidebar('footer_column_3')) { ?>
+        <div class="col-sm-4 footer-column-3">
+          <div class="column-holder">
+            <?php dynamic_sidebar('footer_column_3') ?>
+          </div>
+        </div>
+      <?php } ?>
+    </div>
+  </div>
+</footer>
+
 <?php wp_footer(); ?>
 </body>
+
 </html>
 
 <?php do_action('wp_footer_scripts') ?>
--- includes/_required_files.php
+++ includes/_required_files.php
@@ -2,3 +2,4 @@
 require_once('wp-bootstrap-navwalker.php');
 require_once('menus.php');
 require_once('hooks.php');
+require_once('theme-widgets.php');

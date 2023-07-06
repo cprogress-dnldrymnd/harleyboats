e477071b36aaefaed05b1951b73ece28fc89288e
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-07-06 19:05:07 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -142,3 +142,16 @@ function bbloomer_view_product_button()
 remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30);
 
 
+
+/**
+ * Shop/archives: wrap the product image/thumbnail in a div.
+ * 
+ * The product image itself is hooked in at priority 10 using woocommerce_template_loop_product_thumbnail(),
+ * so priority 9 and 11 are used to open and close the div.
+ */
+add_action('woocommerce_before_shop_loop_item_title', function () {
+	echo '<div class="imagewrapper">';
+}, 9);
+add_action('woocommerce_before_shop_loop_item_title', function () {
+	echo '</div>';
+}, 11);
